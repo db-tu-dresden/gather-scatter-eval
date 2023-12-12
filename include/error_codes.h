de3c7a990c1882738f912464e3e396ba36bda28c
AntonObersteiner <anton.obersteiner@gmail.com>
2023-12-12 16:47:09 +0100

    add basic error handeling

---
+++ include/error_codes.h
@@ -0,0 +1,11 @@
+#ifndef GATHER_ERROR_CODES_H
+#define GATHER_ERROR_CODES_H
+
+enum gather_error_codes {
+	SUCCESS = 0,
+	NO_DATA_SIZE_GIVEN = 1,
+	DATA_SIZE_TOO_LOW = 2,
+	RESULT_FILE_NOT_OPENED = 3,
+};
+
+#endif // include guard GATHER_ERROR_CODES_H
--- src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
+++ src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
@@ -10,6 +10,7 @@
 #include <functional>
 
 #include "gather/simd_variants/avx/agg_avx_64BitVariants.h"
+#include "error_codes.h"
 
 #define ITERATIONS 1
 
@@ -69,11 +70,11 @@ bool benchmark(measures* res, uint64_t correct_result, const uint64_t* values, u
 
 
 int main(int argc, char** argv) {
-	cout << "You are running: " __FILE__ << endl;
+	cerr << "You are running: " __FILE__ << endl;
 
     if (argc < 2) {
-        cout <<"Data Size as input expected!"<<endl;
-        return 0;
+        cerr << "Data Size as input expected (as log_2)!" << endl;
+        return NO_DATA_SIZE_GIVEN;
     }
 
     int p = atoi(argv[1]);
@@ -82,10 +83,23 @@ int main(int argc, char** argv) {
     // 27 --> 134 million integers --> 8GB
     // 26 --> 67 million integers --> 4GB
     uint64_t number_of_values = pow(2, p);
+	cerr << "number_of_values: " << number_of_values << endl;
 
 
     // define max stride size (power of 2)
     size_t max_stride = 15;
+	cerr << "max_stride: " << max_stride << ", 2**max_stride: " << (1 << max_stride) << endl;
+
+	if (max_stride + 1 < p) {
+		/* this stride is fine */
+	} else {
+		cerr
+			<< "Data Size is 2**" << p << " == " << (1<<p)
+			<< " which does not allow the hardcoded maximum stride of "
+			<< "2**" << max_stride << " == " << (1<<max_stride) << "!"
+		<< endl;
+		return DATA_SIZE_TOO_LOW;
+	}
 
     //compute GB for number of values
     double GB = (((double)number_of_values*sizeof(uint64_t)/(double)1024)/(double)1024)/(double)1024;
@@ -110,8 +124,16 @@ int main(int argc, char** argv) {
      */
 
     // open files to store runtime measurements
-     ofstream result_file;
-    result_file.open("./data/gather/avx/single_threaded_64bit_results.dat");
+	string result_filename = "./data/gather/avx/single_threaded_64bit_results.dat";
+    ofstream result_file;
+    result_file.open(result_filename);
+
+	if (result_file.good()) {
+		cout << "writing data to '" << result_filename << "'." << endl;
+	} else {
+		cerr << "writing data to '" << result_filename << "' failed!" << endl;
+		return RESULT_FILE_NOT_OPENED;
+	}
 
     // scalar variant
     if (benchmark(&scalar, correct, array_64, number_of_values, 0, GB, &aggregate_scalar)) {
@@ -166,6 +188,6 @@ int main(int argc, char** argv) {
 
 
 
-  return 0;
+	return SUCCESS;
 }
 
