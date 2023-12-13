720097c314373165682645d0d85127d68f8a77e7
AntonObersteiner <anton.obersteiner@gmail.com>
2023-12-13 17:39:30 +0100

    generalizations for other configurations

--- src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
+++ src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
@@ -1,6 +1,13 @@
-
 #include "common.cpp"
 #include "gather/simd_variants/avx/agg_avx_64BitVariants.h"
+
+constexpr bool multi_threaded = false;
+constexpr bool avx512 = false;
+
+using ResultT = uint64_t;
+
+// 64 bits? else 32 bit integers
+constexpr bool bits64 = std::is_same<ResultT, uint64_t>::value;
 
 int main(int argc, const char** argv) {
     if (argc < 2) {
@@ -10,17 +17,17 @@ int main(int argc, const char** argv) {
 
     int data_size_log2 = atoi(argv[1]);
 
-	const vector<aggregator_t<uint64_t>> aggregators	{
+	const vector<aggregator_t<ResultT>> aggregators	{
 		{ aggregate_scalar,					"scalar",	false },
 		{ aggregate_linear_avx256,			"linear",	false },
 		{ aggregate_strided_gather_avx256,	"gather",	true },
 		{ aggregate_strided_set_avx256,		"seti",		true },
 	};
-	return main_single_threaded<uint64_t>(
+	return main_single_threaded<ResultT>(
 		aggregators,
-		data_size_log2,
-		false,
-		false,
-		true
+		data_size_log2,	// log2 of number of integers
+		multi_threaded,
+		avx512,
+		bits64
 	);
 }
