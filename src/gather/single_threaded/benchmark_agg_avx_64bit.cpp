b650c28283ce3e20fbdfc60648465b6d9b44dceb
AntonObersteiner <anton.obersteiner@gmail.com>
2023-12-13 03:06:00 +0100

    refactor into a general single_threaded version
    otherwise, I will lose my mind in these code duplications

--- src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
+++ src/gather/single_threaded/benchmark_agg_avx_64bit.cpp
@@ -18,26 +18,40 @@
 using namespace std;
 
 #include "measures.h"
-struct measures scalar, linear, gather, seti;
+#include "make_label.cpp"
 
 #include "generate_random_values.cpp"
 // template <ResultT> bool benchmark(...)
 #include "benchmark_single_threaded.cpp"
 
-int main(int argc, char** argv) {
-	cerr << "You are running: " __FILE__ << endl;
+template <class ResultT>
+using aggregation_function_t = uint64_t (*) (
+	const ResultT*,
+	uint64_t,
+	const uint32_t
+);
 
-    if (argc < 2) {
-        cerr << "Data Size as input expected (as log_2)!" << endl;
-        return NO_DATA_SIZE_GIVEN;
-    }
+template <class ResultT>
+struct aggregator {
+	aggregation_function_t<ResultT> function;
+	string label;
+	bool strided;
+};
+template <class ResultT>
+using aggregator_t = struct aggregator<ResultT>;
 
-    int p = atoi(argv[1]);
-
-     // define number of values
+template <class ResultT>
+int main_single_threaded(
+	const vector<aggregator_t<ResultT>> aggregators,
+	uint64_t data_size_log2,
+	bool multi_threaded,
+	bool avx512,
+	bool bits64
+) {
+    // define number of values
     // 27 --> 134 million integers --> 8GB
     // 26 --> 67 million integers --> 4GB
-    uint64_t number_of_values = pow(2, p);
+    uint64_t number_of_values = pow(2, data_size_log2);
 	cerr << "number_of_values: " << number_of_values << endl;
 
 
@@ -45,11 +59,11 @@ int main(int argc, char** argv) {
     size_t max_stride = 15;
 	cerr << "max_stride: " << max_stride << ", 2**max_stride: " << (1 << max_stride) << endl;
 
-	if (max_stride + 1 < p) {
+	if (max_stride + 1 < data_size_log2) {
 		/* this stride is fine */
 	} else {
 		cerr
-			<< "Data Size is 2**" << p << " == " << (1<<p)
+			<< "Data Size is 2**" << data_size_log2 << " == " << (1<<data_size_log2)
 			<< " which does not allow the hardcoded maximum stride of "
 			<< "2**" << max_stride << " == " << (1<<max_stride) << "!"
 		<< endl;
@@ -57,29 +71,38 @@ int main(int argc, char** argv) {
 	}
 
     //compute GB for number of values
-    double GB = (((double)number_of_values*sizeof(uint64_t)/(double)1024)/(double)1024)/(double)1024;
+    double GB = (((double)number_of_values*sizeof(ResultT)/(double)1024)/(double)1024)/(double)1024;
 
 
     /**
      * allocate memory and fill with random numbers
      */
-    uint64_t *array_64;
-    array_64 = (uint64_t *) aligned_alloc(64, number_of_values * sizeof (uint64_t));
-    if (array_64 != NULL) {
+    ResultT *array;
+    array = (ResultT *) aligned_alloc(8 * sizeof (ResultT), number_of_values * sizeof (ResultT));
+    if (array != NULL) {
         cout << "Memory allocated - " << number_of_values << " values" << endl;
     } else {
         cout << "Memory not allocated" << endl;
     }
-    generate_random_values(array_64, number_of_values);
-    uint32_t correct = aggregate_scalar(array_64, number_of_values);
+    generate_random_values(array, number_of_values);
+    uint64_t correct = aggregate_scalar(array, number_of_values);
     cout <<"Generation done."<<endl;
 
     /**
      * run several benchmarks on generated data
      */
 
+	// measurement result structs
+	vector<struct measures> measurements;
+	measurements.assign(aggregators.size(), {0, 0, 0, 0});
+	//struct measures scalar, linear, gather, seti, extra_avx512;
+
     // open files to store runtime measurements
-	string result_filename = "./data/gather/avx/single_threaded_64bit_results.dat";
+	string result_filename = (
+		"./data/gather/"
+		+ make_label(data_size_log2, multi_threaded, avx512, bits64)
+		+ "_results.dat"
+	);
     ofstream result_file;
     result_file.open(result_filename);
 
@@ -90,63 +113,87 @@ int main(int argc, char** argv) {
 		return RESULT_FILE_NOT_OPENED;
 	}
 
-    // scalar variant
-    if (benchmark(&scalar, correct, array_64, number_of_values, 0, GB, &aggregate_scalar)) {
-        cout <<"Scalar done"<<endl;
-    }
-    else {
-        cout <<"scalar failed"<<endl;
-    }
 
-    // avx512 linear load variant
-     if (benchmark(&linear, correct, array_64, number_of_values,0, GB, &aggregate_linear_avx256)) {
-        cout <<"Linear AVX512 done"<<endl;
-    }
-    else {
-        cout <<"Linear AVX512 failed"<<endl;
-    }
+	// note: the stride is the outer loop for the benefit of the output file,
+	// non-strided aggregation methods will still run only once.
+	const int min_stride_pow = 1;
+	for (int stride_pow = min_stride_pow; stride_pow <= max_stride; stride_pow++) {
+		uint64_t stride_size = pow(2, stride_pow);
 
-
-    // strided access evaluation using different strides
-    for (int stride_pow = 1; stride_pow <= max_stride; stride_pow++) {
-        uint64_t stride_size = pow(2, stride_pow);
-
-        // gather instruction
-        if (benchmark(&gather, correct, array_64, number_of_values, stride_size, GB, &aggregate_strided_gather_avx256)) {
-            cout <<"Gather - Stride with Size "<<stride_size<<" done"<<endl;
-        }
-        else {
-            cout <<"Gather - Stride with Size "<<stride_size<<" failed"<<endl;
-        }
-
-        // set instruction
-         if (benchmark(&seti, correct, array_64, number_of_values, stride_size, GB, &aggregate_strided_set_avx512)) {
-            cout <<"Set - Stride with Size "<<stride_size<<" done"<<endl;
-        }
-        else {
-            cout <<"Set - Stride with Size "<<stride_size<<" failed"<<endl;
-        }
-        // writing results to file
-        result_file
+		result_file
 			<< stride_size << " "
-			<< stride_size * 8 << " "
-			<< scalar.mis << " "
-			<< scalar.throughput << " "
-			<< linear.mis << " "
-			<< linear.throughput << " "
-			<< gather.mis << " "
-			<< gather.throughput << " "
-			<< seti.mis << " "
-			<< seti.throughput
-		<< endl;
+			<< stride_size * 8;
+
+		for (int a = 0; a < aggregators.size(); a++) {
+			const aggregation_function_t<ResultT>& function = aggregators[a].function;
+			const string& label = aggregators[a].label;
+			const bool& strided = aggregators[a].strided;
+
+			measures& measurement = measurements[a];
+
+			cerr << "debug! "
+				<< label << " "
+				<< (strided ? "strided" : "not strided") << " with "
+				<< stride_pow << " "
+				<< &measurement
+			<< endl;
+
+			if (!strided) {
+				// those that cannot be strided will not run more than once,
+				// they immediately pass to the next aggregation method.
+				if (stride_pow != min_stride_pow) {
+					continue;
+				} else {
+					stride_size = 0; // will be ignored anyway, just for good measure
+				}
+
+				cout << "running " << label << "... ";
+			} else {
+				// strided access evaluation using different strides
+				cout
+					<< "running " << label
+					<< " with stride 2**" << stride_pow
+					<< " == " << stride_size << "... ";
+			}
+			cerr << "bench" << endl;
+			if (benchmark(&measurement, correct, array, number_of_values, stride_size, GB, function)) {
+				cout << "done." << endl;
+			} else {
+				cout << "FAILED!" << endl;
+			}
+			result_file
+				<< " " << measurement.mis
+				<< " " << measurement.throughput;
+		}
+		result_file << endl;
 	}
     result_file.close();
 
 	cerr << "freeing array!" << endl;
-    free(array_64);
-
-
+    free(array);
 
 	return SUCCESS;
 }
 
+int main(int argc, const char** argv) {
+    if (argc < 2) {
+        cerr << "Data Size as input expected (as log_2)!" << endl;
+        return NO_DATA_SIZE_GIVEN;
+    }
+
+    int data_size_log2 = atoi(argv[1]);
+
+	const vector<aggregator_t<uint64_t>> aggregators	{
+		{ aggregate_scalar,					"scalar",	false },
+		{ aggregate_linear_avx256,			"linear",	false },
+		{ aggregate_strided_gather_avx256,	"gather",	true },
+		{ aggregate_strided_set_avx512,		"seti",		true },
+	};
+	return main_single_threaded<uint64_t>(
+		aggregators,
+		data_size_log2,
+		false,
+		false,
+		true
+	);
+}
